16fdc7b346a02ea5f4e4b746a8923bc0955e59e0
Ali <104462729+AlibekOr@users.noreply.github.com>
2024-06-28 20:58:50 +0500

    Softium Glubus project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,13 @@
 import {View, Text} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
 
 const RootLayout = () => {
     return (
-        <View>
+        <SafeAreaView>
             <Text>
-
+                Hello world
             </Text>
-        </View>
+        </SafeAreaView>
     )
 }
 export default RootLayout
